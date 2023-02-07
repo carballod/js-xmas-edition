9a4df513ab78c1edd3b67b56c26e1bd18f9c848f
Daniela Carballo <danscarballo@gmail.com>
2023-02-07 17:43:32 -0300

    Formulario actualizado con lo visto en la clase 8

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
-const $form = document.querySelector('#carta-a-santa');
-
-const nombre = $form.nombre.value;
-const ciudad = $form.ciudad.value;
-const comportamiento = $form.comportamiento.value;
-const descripcionRegalo = $form["descripcion-regalo"].value;
-
-const contieneSoloLetras = /^[A-z]+$/i;
-
-console.log(nombre);
-console.log(comportamiento);
-
 const validarNombre = function(nombre){
+    const contieneSoloLetras = /^[A-z]+$/i;
+
     if (nombre.length === 0) return 'Este campo debe tener al menos 1 caracter';
     else if (nombre.length > 50) return 'Este campo debe tener menos de 50 caracteres';
     else if (!contieneSoloLetras.test(nombre)) return 'El campo nombre solo acepta letras';
@@ -31,3 +21,84 @@ const validarDescripcionRegalo = function(descripcionRegalo){
 
     return '';
 }
+
+function validarFormulario(event){
+
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form["descripcion-regalo"].value;
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        'descripcion-regalo': errorDescripcionRegalo,
+        nombre: errorNombre,
+        ciudad: errorCiudad
+    };
+
+    const esExito = manejarErrores(errores) === 0;
+
+    if(esExito){
+        $form.className = 'oculto';
+        document.getElementById('#exito').className = '';
+    }
+
+    event.preventDefault();
+}
+
+function manejarErrores(errores){
+
+    // Object.keys(errores) = ['descripcion-regalo': '', nombre: '', ciudad: '']
+    // keys = ['descripcion-regalo': '', nombre: '', ciudad: '']
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector('#errores');
+    let cantidadErrores = 0;
+
+    keys.forEach(function(key){
+        const error = errores[key];
+
+        if(error){
+            cantidadErrores++;
+            $form[key].className = 'error';
+
+            const $error = document.createElement('li');
+            $error.innerText = error;
+            $errores.appendChild($error);
+        } else {
+            $form[key].className = '';
+        }
+
+        return cantidadErrores;
+    });
+
+    // errorNombre = errores.nombre; // nombre;
+    // errorCiudad = errores.ciudad; // ciudad;
+    // errorDescripcionRegalo = errores.descripcionRegalo; // descripcionRegalo
+
+    // if (errorNombre){
+    //     $form.nombre.className = "error";
+    // } else {
+    //     $form.nombre.className = "";
+    // }
+
+    // if (errorCiudad){
+    //     $form.ciudad.className = "error"
+    // } else{
+    //     $form.ciudad.className = ""
+    // }
+
+    // if (errores.descripcionRegalo){
+    //     $form['descripcion-regalo'].className = "error";
+    // } else{
+    //     $form['descripcion-regalo'].className = "";
+    // }
+}
+
+const $form = document.querySelector('#carta-a-santa');
+$form.onsubmit = validarFormulario;
+
+
+
+// CTRL + / para comentar varias lineas de codigo
